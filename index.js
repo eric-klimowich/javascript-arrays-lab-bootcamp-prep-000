a64f1a1d32a50858c170873d739415e1bcc80c15
Eric Klimowich <eklimowich@hotmail.com>
2018-07-18 06:34:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Add your functions and code here
+function destructivelyAppendKitten(name)
+{
+  
+}
+
+function destructivelyPrependKitten(name)
+{
+  
+}
+
+function destructivelyRemoveLastKitten()
+{
+  
+}
+
+function destructivelyRemoveFirstKitten()
+{
+  
+}
+
